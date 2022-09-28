80691a5a1cbfbcdac3efc19b143cb7a718f64498
pinocchio-life-like <icbr19fl@gmail.com>
2022-09-28 15:55:57 +0300

    python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    """
+    Class that inherits from list
+    """
+    def print_sorted(self):
+        """
+        prints list in ascending sort
+        """
+        sort_list = super().copy()
+        sort_list.sort()
+        print(sort_list)
