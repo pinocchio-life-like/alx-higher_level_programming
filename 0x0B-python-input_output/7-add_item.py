7bbe67608f9c26801ef2037f77713166f6d6bbcc
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2022-09-30 10:17:39 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""add item"""
+'''task 7 module'''
 
 
-import json
 import sys
-import os.path
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
-else:
-    obj = []
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, filename)
+arglist = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
+
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
